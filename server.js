a66966e1e8a0a6a99f4e06c71de3a6b94aef8d32
frankcao <1666930632@qq.com>
2023-08-02 15:09:21 +0800

    feat: 🎸 router and async component

--- server.js
+++ server.js
@@ -1,94 +1,147 @@
 import escapeHtml from 'escape-html'
-import { readFile } from 'fs/promises'
+import { readFile, readdir } from 'fs/promises'
 import { createServer } from 'http'
-
+import sanitizeFilename from 'sanitize-filename'
 createServer(async (req, res) => {
-  const author = 'Jae Doe'
-  const postContent = await readFile('./posts/hello-world.txt', 'utf8')
-  sendHTML(
-    res,
+  try {
+    const url = new URL(req.url, `http://${req.headers.host}`)
+
+    await sendHTML(res, <Route url={url} />)
+  } catch (err) {
+    console.error(err)
+    res.statusCode = err.statusCode ?? 500
+    res.end()
+  }
+}).listen(8080)
+
+async function sendHTML(res, jsx) {
+  const html = await renderJSXToHTML(jsx)
+  res.setHeader('Content-Type', 'text/html')
+  res.end(html)
+}
+
+
+function Route({ url }) {
+  let page
+  if (url.pathname === '/') {
+    page = <BlogIndexPage />
+  } else {
+    const slug = sanitizeFilename(url.pathname.slice(1))
+    page = <BlogPostPage slug={slug} />
+  }
+  return <BlogLayout>{page}</BlogLayout>
+}
+
+function Footer({ author }) {
+  return (
+    <footer>
+      <hr />
+      <p>
+        <i>
+          (c) {author} {new Date().getFullYear()}
+        </i>
+      </p>
+    </footer>
+  )
+}
+
+function BlogLayout({ children }) {
+  const author = 'frankcao'
+  return (
     <html>
       <head>
         <title>My blog</title>
       </head>
       <body>
         <nav>
-          <a href="/">Home</a>
+          <a href="/">home</a>
           <hr />
         </nav>
-        <article style={{backgroundColor: 'black'}}>{postContent}</article>
-        <footer>
-          <hr />
-          <p>
-            <i>
-              (c) {author} {new Date().getFullYear()}
-            </i>
-          </p>
-        </footer>
+        <main>{children}</main>
+        <Footer author={author} />
       </body>
     </html>
   )
-}).listen(8080)
-
-function sendHTML(res, jsx) {
-  const html = renderJSXToHTML(jsx)
-  res.setHeader('Content-Type', 'text/html')
-  res.end(html)
 }
 
-function renderJSXToHTML(jsx) {
-  console.log(jsx)
+async function BlogIndexPage({}) {
+  const files = await readdir('./posts')
+  const slugs = files.map((file) => file.slice(0, file.lastIndexOf('.')))
+  return (
+    <section>
+      <h1>Welcome to my blog</h1>
+      <div>
+        {slugs.map((slug) => (
+          <Post key={slug} slug={slug} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+async function Post({ slug }) {
+  let content
+  try {
+    content = await readFile('./posts/' + slug + '.txt', 'utf8')
+  } catch (err) {
+    throwPageNotFound(err)
+  }
+  return (
+    <section>
+      <h2>
+        <a href={'/' + slug}>{slug}</a>
+      </h2>
+      <article>{content}</article>
+    </section>
+  )
+}
+
+function BlogPostPage({ slug }) {
+  console.log('BlogPostPage: slug', slug)
+  return <Post slug={slug} />
+}
+
+function throwPageNotFound(cause) {
+  const notFound = new Error('Page Not found.', { cause })
+  notFound.statusCode = 404
+  throw notFound
+}
+
+
+async function renderJSXToHTML(jsx) {
+  // console.log(jsx)
   if (typeof jsx === 'string' || typeof jsx === 'number') {
     return escapeHtml(jsx)
   } else if (jsx == null || typeof jsx === 'boolean') {
     return ''
   } else if (Array.isArray(jsx)) {
-    return jsx.map((child) => renderJSXToHTML(child)).join('')
+    //map 里不能await
+    // return jsx.map((child) => (await renderJSXToHTML(child))).join('')
+    const child = await Promise.all(jsx.map((child) => renderJSXToHTML(child)))
+    return child.join('')
   } else if (typeof jsx === 'object') {
     if (jsx.$$typeof === Symbol.for('react.element')) {
-      let html = '<' + jsx.type
-      for (let key in jsx.props) {
-        if (jsx.props.hasOwnProperty(key) && key !== 'children') {
-          html += ' ' + key + '='
-          html += escapeHtml(jsx.props[key])
+      // 原生html标签
+      if (typeof jsx.type === 'string') {
+        let html = '<' + jsx.type
+        for (let key in jsx.props) {
+          if (jsx.props.hasOwnProperty(key) && key !== 'children') {
+            html += ' ' + key + '='
+            html += escapeHtml(jsx.props[key])
+          }
         }
+        html += '>'
+        html += await renderJSXToHTML(jsx.props.children)
+        html += '</' + jsx.type + '>'
+        return html
+
+        //react 组件
+      } else if (typeof jsx.type === 'function') {
+        const Component = jsx.type
+        const retJsx = await Component(jsx.props)
+        console.log('function component ', renderJSXToHTML(retJsx))
+        return renderJSXToHTML(retJsx)
       }
-      html += '>'
-      html += renderJSXToHTML(jsx.props.children)
-      html += '</' + jsx.type + '>'
-      return html
     } else throw new Error('Cannot render an object.')
   } else throw new Error('Not implemented.')
-}
-
-function BlogLayout({ children }) {
-  const author = "Jae Doe";
-  return (
-    <html>
-      <head>
-        <title>My blog</title>
-      </head>
-      <body>
-        <nav>
-          <a href="/">Home</a>
-          <hr />
-        </nav>
-        <main>
-          {children}
-        </main>
-        <Footer author={author} />
-      </body>
-    </html>
-  );
-}
-
-function BlogPostPage({ postSlug, postContent }) {
-  return (
-    <section>
-      <h2>
-        <a href={"/" + postSlug}>{postSlug}</a>
-      </h2>
-      <article>{postContent}</article>
-    </section>
-  );
 }
